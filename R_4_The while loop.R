244be1899931d9d7cff822e02c39c852dcce3029
Sneha1123 <49165714+Sneha1123@users.noreply.github.com>
2019-11-19 16:37:55 +0530

    Add files via upload

---
+++ R_4_The while loop.R
@@ -0,0 +1,19 @@
+#  while(<logical expression>){
+#   <body of loop>
+# }
+
+while(FALSE){
+  print("Hello")
+}
+
+while(TRUE){
+  print("Hello")
+}
+
+#esc - to stop infinite loop
+
+ counter <- 1
+ while (counter < 5) {
+   print(counter)
+   counter <- counter+1
+ }
